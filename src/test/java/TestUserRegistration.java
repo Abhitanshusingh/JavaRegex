27b95733070427951af3383931c6274c9636dce7
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-05 17:06:31 +0530

    ADDED: Passes test case

--- src/test/java/TestUserRegistration.java
+++ src/test/java/TestUserRegistration.java
@@ -1,5 +1,18 @@
+import org.junit.Assert;
 import org.junit.Test;
 public class TestUserRegistration {
-    UserRegistration us =new UserRegistration();
+    UserRegistration validator =new UserRegistration();
+    //TEST CASE FOR VALID FIRST NAME
+    @Test
+    public void firstNameValid() {
+        boolean result=validator.isValidFirstName("Abhi");
+        Assert.assertTrue(result);
+    }
+    //TEST CASE FOR INVALID VALID FIRST NAME
+    @Test
+    public void firstNameInvalid() {
+        boolean result=validator.isValidFirstName("abhi");
+        Assert.assertFalse(result);
+    }
 
 }
