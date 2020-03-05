51a524c3c1777dca82a00ad8ea6652cec61b3d0c
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-05 18:06:54 +0530

    MODIFIED: Added cheching email is valid or invalid

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -5,6 +5,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     public static void main(String[] args) {
         System.out.println("Welcome to user registration");
+        //PRINTING RESULT OF FIRST NAME
         System.out.println("Enter first name: ");
         Scanner sc=new Scanner(System.in);
         String firstName=sc.next();
@@ -13,14 +14,22 @@ public class UserRegistration {
             System.out.println("First name is valid");
         else
             System.out.println("First name not valid");
-
+        //PRINTING RESULT OF LASTNAME
         System.out.println("Enter last name: ");
         String lastName=sc.next();
-        boolean  lastNameResult=isValidFirstName(lastName);
+        boolean  lastNameResult=isValidLastName(lastName);
         if(lastNameResult)
             System.out.println("last name is valid");
         else
             System.out.println("Last name invalid ");
+        //PRINTING RESULT OF EMAIL
+        System.out.println("Enter email: ");
+        String email=sc.next();
+        boolean  emailResult=isValidEmail(email);
+        if(emailResult)
+            System.out.println("email is valid");
+        else
+            System.out.println("email invalid ");
     }
     //CHECKING VALID OR INVALID FIRST NAME
     public static boolean isValidFirstName(String firstName){
@@ -37,4 +46,13 @@ public class UserRegistration {
         Matcher mchr = ptrn.matcher(lastName);
         return mchr.matches();
     }
+
+    //CHECKING VALID OR INVALID EMAIL
+    public static boolean isValidEmail(String email){
+        String emailPattern="^[a-z]{1,}([.]?[-]?[+]?[a-z0-9]{1,})?[@]{1}[a-z0-9]{1,}[.]{1}[a-z]{2,4}([.]?[a-z]{2,4})?$";
+        Pattern ptrn = Pattern.compile(emailPattern);
+        Matcher mchr = ptrn.matcher(email);
+        return mchr.matches();
+    }
+
 }
