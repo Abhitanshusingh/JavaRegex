367308ff3680e97b9875a290305d82e0727aa6e3
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-05 18:47:07 +0530

    MODIFIED: Added to validate phone number test case

--- src/test/java/TestUserRegistration.java
+++ src/test/java/TestUserRegistration.java
@@ -1,41 +1,61 @@
 import org.junit.Assert;
 import org.junit.Test;
 public class TestUserRegistration {
-    UserRegistration validator =new UserRegistration();
+    UserRegistration validator = new UserRegistration();
+
     //TEST CASE FOR VALID FIRST NAME
     @Test
     public void firstNameValid() {
-        boolean result=validator.isValidFirstName("Abhi");
+        boolean result = validator.isValidFirstName("Abhi");
         Assert.assertTrue(result);
     }
+
     //TEST CASE FOR INVALID VALID FIRST NAME
     @Test
     public void firstNameInvalid() {
-        boolean result=validator.isValidFirstName("abhi");
+        boolean result = validator.isValidFirstName("abhi");
         Assert.assertFalse(result);
     }
+
     //TEST CASE FOR VALID LAST NAME
     @Test
     public void lastNameValid() {
-        boolean result=validator.isValidLastName("Singh");
+        boolean result = validator.isValidLastName("Singh");
         Assert.assertTrue(result);
     }
+
     //TEST CASE FOR INVALID VALID LAST NAME
     @Test
     public void lastNameInvalid() {
-        boolean result=validator.isValidLastName("sInGh");
+        boolean result = validator.isValidLastName("sInGh");
         Assert.assertFalse(result);
     }
-    //TEST CASE FOR VALID email
+
+    //TEST CASE FOR VALID EMAIL
     @Test
     public void isemailalid() {
-        boolean result=validator.isValidEmail("abhitanshusingh99@gmail.com");
+        boolean result = validator.isValidEmail("abhitanshusingh99@gmail.com");
         Assert.assertTrue(result);
     }
-    //TEST CASE FOR INVALID VALID LAST NAME
+
+    //TEST CASE FOR INVALID VALID EMAIL
     @Test
     public void isemailInvalid() {
-        boolean result=validator.isValidEmail("99@gmail.com");
+        boolean result = validator.isValidEmail("99@gmail.com");
+        Assert.assertFalse(result);
+    }
+
+    //TEST CASE FOR VALID PHONE NUMBER
+    @Test
+    public void isPhobneNumberVlalid() {
+        boolean result = validator.isValidPhoneNumber("91 1234567890");
+        Assert.assertTrue(result);
+    }
+
+    //TEST CASE FOR INVALID PHONE NUMBER
+    @Test
+    public void isPhoneNumberInvalid() {
+        boolean result = validator.isValidEmail("19123");
         Assert.assertFalse(result);
     }
 }
