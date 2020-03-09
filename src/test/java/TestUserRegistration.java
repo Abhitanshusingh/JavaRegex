791e8c414f783e12a75faa19811884a2a31f2bde
Abhitanshusingh <abhitanshusingh99@gmail.com>
2020-03-09 18:25:00 +0530

    REFACTORED: Changed all test case method name

--- src/test/java/TestUserRegistration.java
+++ src/test/java/TestUserRegistration.java
@@ -1,129 +1,118 @@
 import org.junit.Assert;
 import org.junit.Test;
+
 public class TestUserRegistration {
     UserRegistration validator = new UserRegistration();
 
-    //TEST CASE FOR VALID FIRST NAME
     @Test
-    public void firstNameValid() {
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
         boolean result = validator.isValidFirstName("Abhi");
         Assert.assertTrue(result);
     }
 
-    //TEST CASE FOR INVALID VALID FIRST NAME
     @Test
-    public void firstNameInvalid() {
+    public void givenFirstName_WhenProper_ShouldReturnFalse() {
         boolean result = validator.isValidFirstName("abhi");
         Assert.assertFalse(result);
     }
 
-    //TEST CASE FOR VALID LAST NAME
     @Test
-    public void lastNameValid() {
+    public void givenLastName_WhenProper_ShouldReturnTrue() {
         boolean result = validator.isValidLastName("Singh");
         Assert.assertTrue(result);
     }
 
-    //TEST CASE FOR INVALID VALID LAST NAME
     @Test
-    public void lastNameInvalid() {
+    public void givenLastName_WhenProper_ShouldReturnFalse() {
         boolean result = validator.isValidLastName("sInGh");
         Assert.assertFalse(result);
     }
 
-    //TEST CASE FOR VALID EMAIL
     @Test
-    public void isemailalid() {
+    public void givenEmail_WhenProper_ShouldReturnTrue() {
         boolean result = validator.isValidEmail("abhitanshusingh99@gmail.com");
         Assert.assertTrue(result);
     }
 
-    //TEST CASE FOR INVALID VALID EMAIL
     @Test
-    public void isemailInvalid() {
+    public void givenEmail_WhenProper_ShouldReturnFalse() {
         boolean result = validator.isValidEmail("99@gmail.com");
         Assert.assertFalse(result);
     }
 
-    //TEST CASE FOR VALID PHONE NUMBER
     @Test
-    public void isPhobneNumberVlalid() {
+    public void givenPhoneNumber_WhenProper_ShouldReturnTrue() {
         boolean result = validator.isValidPhoneNumber("91 1234567890");
         Assert.assertTrue(result);
     }
 
-    //TEST CASE FOR INVALID PHONE NUMBER
     @Test
-    public void isPhoneNumberInvalid() {
+    public void givenPhoneNumber_WhenProper_ShouldReturnFalse() {
         boolean result = validator.isValidEmail("19123");
         Assert.assertFalse(result);
     }
-    //TEST CASE FOR VALID PASSWORD MINIMUM 8 CHARACTER
+
     @Test
-    public void isPasswordVlalid() {
+    public void givenMinimumEightCharacterPassword_WhenProper_ShouldReturnTrue() {
         boolean result = validator.isValidPassword("abhi1234");
         Assert.assertTrue(result);
     }
 
-    //TEST CASE FOR INVALID PASSWORD MINIMUM 8 CHARACTER
     @Test
-    public void isPasswordInvalid() {
+    public void givenMinimumEightCharacterPassword_WhenProper_ShouldReturnFalse() {
         boolean result = validator.isValidPassword("ab123");
         Assert.assertFalse(result);
     }
-    //TEST CASE FOR VALID PASSWORD ONE UPPER CASE
+
     @Test
-    public void isOneUpperCaseValid(){
+    public void givenOneUpperCasePassword_WhenProper_ShouldReturnTrue() {
         boolean result = validator.isOneUpperCaseValidPassword("Abhitanshu");
         Assert.assertTrue(result);
     }
 
-    //TEST CASE FOR INVALID PASSWORD MINIMUM 8 CHARACTER
     @Test
-    public void isOneUpperCaseInValid() {
+    public void givenOneUpperCasePassword_WhenProper_ShouldReturnFalse() {
         boolean result = validator.isOneUpperCaseValidPassword("abhitanshu");
         Assert.assertFalse(result);
     }
-    //TEST CASE FOR VALID PASSWORD ONE NUMERIC
+
     @Test
-    public void isOneNumericValid(){
+    public void givenOneNumericPassword_WhenProper_ShouldReturnTrue() {
         boolean result = validator.isOneNumericValidPassword("Abhitanshu99");
         Assert.assertTrue(result);
     }
 
-    //TEST CASE FOR INVALID PASSWORD ONE NUMERIC
     @Test
-    public void isOneNumericInValid() {
+    public void givenOneNumericPassword_WhenProper_ShouldReturnFalse() {
         boolean result = validator.isOneNumericValidPassword("Abhitanshu");
         Assert.assertFalse(result);
     }
-    //TEST CASE FOR VALID PASSWORD EXACTLY ONE SPECIAL CHARACTER
+
     @Test
-    public void isSpecialCharacterValid(){
+    public void givenExactlyOneSpecialCharacterPassword_WhenProper_ShouldReturnTrue() {
         boolean result = validator.isSpecialValidPassword("Abhitanshu99@");
         Assert.assertTrue(result);
     }
 
-    //TEST CASE FOR INVALID PASSWORD EXACTLY ONE SPECIAL CHARACTER
     @Test
-    public void isSpecialCharacterInValid() {
+    public void givenExactlyOneSpecialCharacterPassword_WhenProper_ShouldReturnFalse() {
         boolean result = validator.isSpecialValidPassword("Abhitanshu99@@");
         Assert.assertFalse(result);
     }
-    //TEST CASE FOR ALL VALID EMAIL
+
     @Test
-    public void isAllEmailValid(){
-        String[] validEmailArray= {"abc@yahoo.com" ,"abc-100@yahoo.com","abc.100@yahoo.com","abc111@abc.com","abc-100@abc.net","abc.100@abc.com.au","abc@1.com","abc@gmail.com.com"};
-        for (int index=0; index<validEmailArray.length; index++) {
+    public void givenEmailPassword_WhenProper_ShouldReturnTrue() {
+        String[] validEmailArray = {"abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com"};
+        for (int index = 0; index < validEmailArray.length; index++) {
             boolean result = validator.isValidEmail(validEmailArray[index]);
             Assert.assertTrue(result);
         }
     }
-    //TEST CASE FOR ALL INVALID EMAIL
+
     @Test
-    public void isAllEmailInValid(){
-        String[] InvalidEmailArray= {"abc","abc@.com.my","abc123@gmail.a","abc123@.com","abc123@.com.com",".abc@abc.com","abc()*@gmail.com","abc@%*.com","abc..2002@gmail.com","abc.@gmail.com ","abc@abc@gmail.com","abc@gmail.com.1a","abc@gmail.com.aa.au"};
-        for (int index=0; index<InvalidEmailArray.length; index++) {
+    public void givenEmailPassword_WhenProper_ShouldReturnFalse() {
+        String[] InvalidEmailArray = {"abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com ", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au"};
+        for (int index = 0; index < InvalidEmailArray.length; index++) {
             boolean result = validator.isValidEmail(InvalidEmailArray[index]);
             Assert.assertFalse(result);
         }
